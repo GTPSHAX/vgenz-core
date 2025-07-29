7fa151ef67a5b876cf27812e349fc929cef7af0e
GTPSHAX <rafiehasannudin@gmail.com>
2025-07-29 10:22:02 -0700

    Tu: 0:9/ 10:21

--- bin/https/Config.js
+++ bin/https/Config.js
@@ -1,14 +1,14 @@
 module.exports = {
     beta: {
         allow_ips: ["::ffff:103.51.205.190", "::ffff:139.0.108.58", "::ffff:139.0.228.161", "::ffff:139.0.105.32"],
-        server_port: 17092,
-        server_ip: "152.42.200.71"
+        server_port: 17096,
+        server_ip: "68.183.236.202"
     },
 
     // your server name
     server_name: "Growtopia",
     // public ipv4
-    server_ip: "152.42.200.71",
+    server_ip: "68.183.236.202",
     // UDP port
     server_port: 17096,
     // login url dashboard
